373a736e0261d6302eb82527df4ee47ef0d307a6
Samuel Andrade <samuel.aas.2005@gmail.com>
2025-05-07 15:13:07 -0300

    Update maincode.py

--- maincode.py
+++ maincode.py
@@ -1,73 +1,89 @@
 print('\t\t\t\t\t\tAntifake')
+
+flag = True
 while flag:
     cadastro = input('Já tem uma conta? (s)im (n)ão: ').strip().lower()
+    
     if cadastro.startswith('s'):
-        ...
-    elif cadastro.startswith('n'): # Se o usuário ainda não se cadastrou
-        flag = True
-        print('Cadastre-se agora!')
-        
-        while True:
-            nome = input("Digite seu nome completo: ").strip()
+        print("Login em desenvolvimento...")
+        flag = False
 
-            # Verifica se o nome contém apenas letras e espaços
+    elif cadastro.startswith('n'):
+        print('Cadastre-se agora!')
+
+        # Nome
+        nome_valido = False
+        while not nome_valido:
+            nome = input("Digite seu nome completo: ").strip()
             if all(c.isalpha() or c.isspace() for c in nome):
-                if len(nome.split()) >= 2:  # Verifica se tem pelo menos nome e sobrenome
-                    break
+                if len(nome.split()) >= 2:
+                    nome_valido = True
                 else:
                     print("Nome inválido, digite seu nome completo.")
             else:
                 print("Nome inválido! Use apenas letras e espaços.")
-        
-        while True:
-            email = input('digite seu email: ').strip()
-            if ' ' in email: # caso ele tenha colocado espaço no email
-                print('email inválido, seu email tem espaços.')
-                continue
-            if '@' not in email: # caso ele não tenha colocado o @ no email
-                print('email inválido, seu email não tem "@".')
-                continue
-            if not ('gmail.com' in email or 'ufrpe.br' in email):  # caso ele não tenha colocado nem gmail.com nem ufrpe.br
-                print('email inválido, use os domínios "gmail.com" ou "ufrpe.br".')
-                continue
-            if len(email.split('@')[0]) < 3: # Se seu email tiver menos de 3 caracteres (antes do @), é inválido
-                print('email inválido, tente novamente!')
-                continue     
-            else:
-                break
-        
-        while True:
-            data_de_nascimento = input('Digite a sua data de nascimento:(dd/mm/aaaa) ').strip()
-            if '/' not in data_de_nascimento:
-                print('formato inválido, use dd/mm/aaaa.')
-                continue
 
-            partes = data_de_nascimento.split('/')
-            if len(partes) != 3:
-                print('formato inválido, use dd/mm/aaaa.')
-                continue
-
-            dia, mes, ano = partes
-            if not (dia.isdigit() and mes.isdigit() and ano.isdigit()):
-                print('A data de nascimento deve conter apenas numeros e "/" para divisão.')
-                continue
-            if len(dia) != 2 or len(mes) != 2 or len(ano) != 4:
-                print('É necessário dois caracteres no dia, dois caracteres no mês e quatro no ano')
-                continue
+        # Email
+        email_valido = False
+        while not email_valido:
+            email = input('Digite seu email: ').strip()
+            if ' ' in email:
+                print('Email inválido, contém espaços.')
+            elif '@' not in email:
+                print('Email inválido, falta "@".')
+            elif not ('gmail.com' in email or 'ufrpe.br' in email):
+                print('Email inválido, use "gmail.com" ou "ufrpe.br".')
+            elif len(email.split('@')[0]) < 3:
+                print('Email inválido, parte antes do "@" muito curta.')
             else:
-                break
+                email_valido = True
 
-        while True:
-            senha = input('Digite sua senha (min 8 caracteres): ')
-            if len(senha) < 8:
-                print('caracteres insuficientes, deve ter no mínimo 8.')
-                continue
-            confirmação_senha = input('confirme sua senha: ')
-            if confirmação_senha == senha:
-                break
+        # Data de nascimento
+        def eh_bissexto(ano):
+            return ano % 4 == 0 and (ano % 100 != 0 or ano % 400 == 0)
+
+        def dias_no_mes(mes, ano):
+            if mes == 2:
+                return 29 if eh_bissexto(ano) else 28
+            elif mes in [4, 6, 9, 11]:
+                return 30
             else:
-                print('A confirmação falhou, suas senhas foram diferentes!')
+                return 31
+
+        data_valida = False
+        while not data_valida:
+            try:
+                dia = int(input("Digite o seu dia de nascimento: "))
+                mes = int(input("Digite o seu mês de nascimento: "))
+                ano = int(input("Digite o seu ano de nascimento: "))
+
+                if ano < 1900:
+                    print("Ano inválido.")
+                elif mes < 1 or mes > 12:
+                    print("Mês inválido.")
+                elif dia < 1 or dia > dias_no_mes(mes, ano):
+                    print("Dia inválido.")
+                else:
+                    print(f"Data válida: {dia:02}/{mes:02}/{ano}")
+                    data_valida = True
+            except ValueError:
+                print("Digite apenas números válidos.")
+
+        # Senha
+        senha_valida = False
+        while not senha_valida:
+            senha = input('Digite sua senha (mínimo 8 caracteres): ')
+            if len(senha) >= 8:
+                confirmacao = input('Confirme sua senha: ')
+                if confirmacao == senha:
+                    senha_valida = True
+                else:
+                    print('As senhas não coincidem.')
+            else:
+                print('Senha muito curta.')
+
+        print("Cadastro realizado com sucesso!")
+        flag = False
 
     else:
-        print('Digite apenas "s" ou "n"')
-        continue
+        print('Digite apenas "s" ou "n".')
